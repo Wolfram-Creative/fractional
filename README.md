d285aa1273f719ee4ba84407423e0abadd538c68
Nate Lubeck <nate@wolframcreative.com>
2014-03-30 17:29:12 -0700

    - PhantomCSS support added. Gives screenshots of mobile and desktop breakpoints. - Added server and test tasks to grunt - Need to figure out better way of storing screenshots to avoid potential repo bloat.

--- README.md
+++ README.md
@@ -5,4 +5,7 @@ A fractional css grid system
 
 View live site and examples here: http://fractional.wolframcreative.com
 
+
+Added support for PhantomCSS - you can now test responsive.
+
 Enjoy!
---
+++ test/visual/baseline.js
@@ -0,0 +1,22 @@
+casper.start('http://localhost:8181');
+
+casper.then(function () {
+	phantomcss.screenshot('body', 'Baseline');
+});
+
+casper.then( function now_check_the_screenshots(){
+	// compare screenshots
+	phantomcss.compareAll();
+});
+
+casper.then( function end_it(){
+	casper.test.done();
+});
+
+/*
+Casper runs tests
+*/
+casper.run(function(){
+	console.log('\nTHE END.');
+	phantom.exit(phantomcss.getExitStatus());
+});
